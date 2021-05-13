adc6874fd8b58f2e7758d7a0bdc58c8676b2cde4
Michael Wedd <michael.wedd@leverege.com>
2021-05-13 00:40:56 -0400

    BUILD add in airtable base configs until new api server is live

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,12 @@
 module.exports = {
     REALM_APP_ID: process.env.REACT_APP_REALM_APP_ID,
     STRIPE_API_KEY : process.env.REACT_APP_STRIPE_API_KEY,
+    AIRTABLE_API_KEY : process.env.REACT_APP_AIRTABLE_API_KEY,
+    AIRTABLE_BASE_ID : process.env.REACT_APP_AIRTABLE_BASE_ID,
     STRIPE_SERVER_URL : process.env.REACT_APP_STRIPE_SERVER_URL,
     AIRTABLE_SERVER_URL : process.env.REACT_APP_AIRTABLE_SERVER_URL,
+    AIRTABLES : {
+        PEOPLE : 'People',
+        BOOKINGS : 'Bookings',
+    }
 }
