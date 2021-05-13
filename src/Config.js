cd9795426f78d943a5cf9fbc7b2e969dafbaa49e
Michael Wedd <michael.wedd@leverege.com>
2021-05-13 00:27:40 -0400

    BUILD add config vars from .env

--- src/Config.js
+++ src/Config.js
@@ -1,12 +1,6 @@
 module.exports = {
-    REALM_APP_ID: process.env.REALM_APP_ID,
-    STRIPE_API_KEY : process.env.STRIPE_API_KEY,
-    AIRTABLE_API_KEY : process.env.AIRTABLE_API_KEY,
-    AIRTABLE_BASE_ID : process.env.AIRTABLE_BASE_ID,
-    STRIPE_SERVER_URL : process.env.STRIPE_SERVER_URL || 'https://ccc-stripe-checkout.glitch.me',
-    AIRTABLES : {
-        PEOPLE : 'People',
-        BOOKINGS : 'Bookings',
-    }
-    // more configs
+    REALM_APP_ID: process.env.REACT_APP_REALM_APP_ID,
+    STRIPE_API_KEY : process.env.REACT_APP_STRIPE_API_KEY,
+    STRIPE_SERVER_URL : process.env.REACT_APP_STRIPE_SERVER_URL,
+    AIRTABLE_SERVER_URL : process.env.REACT_APP_AIRTABLE_SERVER_URL,
 }
